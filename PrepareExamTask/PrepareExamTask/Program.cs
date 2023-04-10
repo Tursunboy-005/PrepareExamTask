1fa01dce3ebe77038f31b4c7d39018d00a9ff4d0
Tursunboy <tursunboyrahimjonov1@gmail.com>
2023-04-10 09:32:45 +0500

    Raqamlar yigindisi

--- PrepareExamTask/PrepareExamTask/Program.cs
+++ PrepareExamTask/PrepareExamTask/Program.cs
@@ -10,6 +10,27 @@ namespace PrepareExamTask
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number : ");
+            int n = int.Parse(Console.ReadLine());
+           
+
+            Console.WriteLine(Summa(n));
+            Console.ReadLine();
+            Console.ReadKey();
+            
         }
+        public static int Summa(int n)
+        {
+            int sum = 0;
+            int qoldiq = 0;
+            while(n != 0)
+            {
+                qoldiq = n % 10;
+                sum += qoldiq;
+                n /= 10;
+            }
+            return sum;
+        }
+
     }
 }
